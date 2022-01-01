992a6b4a331ccc5904277d2ec939b951b18733a6
Aditya99111 <adityamittal471@gmail.com>
2022-01-01 21:16:56 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import './App.css';
+import {BrowserRouter as Router , Route } from "react-router-dom"
+import Landing from './Landing';
+import Cart from "./Components/Cart/Cart"
+import AboutPage from "./Components/About/AboutPage"
+import { commerce } from "./lib/commerce"
+import Products from './Components/Products/Products';
+function App() {
+
+
+  const [products, setproducts] = useState([])
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list()
+
+    setproducts(data)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+console.log(products)
+return (
+    <div>
+
+    <Router>
+    <Route exact path="/" component={Landing}/>
+    <Route exact path="/cart" component={Cart}/>
+    <Route exact path="/products" component={Products}/>
+    <Route exact path="/about" component={AboutPage}/>
+
+     </Router>
+    </div>
+  );
+}
+
+export default App;
