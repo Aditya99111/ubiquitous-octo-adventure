2705121da4888534069b7068c21fa30cb5ab25c0
john Cervantes <johncamilocervantes@gmail.com>
2022-01-04 10:19:38 -0600

    fixed displaying of products in products page

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import Product from './Product'
-import Product1 from "../../Assets/product1.jpg"
+import React from "react";
+import Product from "./Product";
+import Product1 from "../../Assets/product1.jpg";
 
-const Products = ({products}) => {
-console.log(products)
+const Products = ({ products }) => {
+  console.log(products);
   return (
-
     <div className="productspage">
-      <h1 className="heading">our <span>products</span></h1>
-      
-       <Product
-        title="hello"
-        id="1"
-        discount="22%"
-        desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus qui minus alias odit expedita quisquam in autem, nulla inventore enim tenetur magnam. Excepturi commodi repellendus recusandae distinctio alias quo! Harum."
-        image={Product1}
-        price="99"
-        price_before_discount="110"
-      />
-
+      <h1 className="heading">
+        our <span>products</span>
+      </h1>
+      {products.map((product) => {
+        return (
+          <Product
+            title={product.name}
+            id={product.id}
+            discount="22%"
+            desc={product.description.replace(/(<([^>]+)>)/gi, "")}
+            image={product.image.url}
+            price={product.price.raw}
+            price_before_discount="110"
+          />
+        );
+      })}
     </div>
-  )
-}
-export default Products
+  );
+};
+export default Products;
