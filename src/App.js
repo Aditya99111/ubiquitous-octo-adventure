86339c5830752c3346f4dabebfd831032e4f5e3e
john Cervantes <johncamilocervantes@gmail.com>
2022-01-06 14:35:57 -0600

    fixed cart problems

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import Landing from './Landing';
-import Cart from "./Components/Cart/Cart"
-import AboutPage from "./Components/About/AboutPage"
-import { commerce } from "./lib/commerce"
-import Products from './Components/Products/Products';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Landing from "./Landing";
+import Cart from "./Components/Cart/Cart";
+import AboutPage from "./Components/About/AboutPage";
+import { commerce } from "./lib/commerce";
+import Products from "./Components/Products/Products";
 function App() {
-  const [products, setproducts] = useState([])
+  const [products, setproducts] = useState([]);
 
-  const [cart, setCart] = useState({})
+  const [cart, setCart] = useState({});
   const fetchProducts = async () => {
-    const { data } = await commerce.products.list()
+    const { data } = await commerce.products.list();
 
-    setproducts(data)
-  }
+    setproducts(data);
+  };
 
   const fetchCart = async () => {
-    setCart(await commerce.cart.retrieve())
-  }
+    setCart(await commerce.cart.retrieve());
+  };
 
   const handleAddToCart = async (productId, quantity) => {
     const item = await commerce.cart.add(productId, quantity);
     setCart(item.cart);
-  }
+    console.log(cart);
+  };
 
   useEffect(() => {
-    fetchProducts()
-    fetchCart()
-  }, [])
+    fetchProducts();
+    fetchCart();
+  }, []);
 
-  console.log(cart)
   return (
     <div>
-
       <Router>
         <Route exact path="/" component={Landing} />
         <Route exact path="/cart" component={Cart} />
--- src/Components/Products/Product.js
+++ src/Components/Products/Product.js
@@ -1,35 +1,38 @@
-import React from 'react'
-import "./product.css"
+import React from "react";
+import "./product.css";
 
-const Product = (props,{OnAddToCart, products}) => {
-console.log(products)
-    return (
-        <>
-            <div className="product" id={props.id} >
-                <div className="product-img">
-                    <img src={props.image} alt="" />
-                    <span className="tag">{props.discount} off</span>
-                </div>
-                <div className="product-listing">
-                    <div className="content">
-                        <h1 className="name">{props.title}</h1>
-                        <p className="info">{props.desc}</p>
-                        <p className="price">${props.price} <span>${props.price_before_discount}</span></p>
-                        <div className="btn-and-rating-box">
-                            <div className="rating">
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star-half-alt"></i>
-                            </div>
-                            <button onClick={()=> OnAddToCart(products.id,1)} className="btn">Add to cart <i className="fas fa-shopping-cart"></i></button>
-                        </div>
-                    </div>
-                </div>
+const Product = (props) => {
+  return (
+    <>
+      <div className="product" id={props.id}>
+        <div className="product-img">
+          <img src={props.image} alt="" />
+          <span className="tag">{props.discount} off</span>
+        </div>
+        <div className="product-listing">
+          <div className="content">
+            <h1 className="name">{props.title}</h1>
+            <p className="info">{props.desc}</p>
+            <p className="price">
+              ${props.price} <span>${props.price_before_discount}</span>
+            </p>
+            <div className="btn-and-rating-box">
+              <div className="rating">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star-half-alt"></i>
+              </div>
+              <button onClick={() => props.OnAddToCart(props.id, 1)} className="btn">
+                Add to cart <i className="fas fa-shopping-cart"></i>
+              </button>
             </div>
-        </>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Product
+export default Product;
