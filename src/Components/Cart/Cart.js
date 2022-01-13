c585a8f2ddacc54f8374db31a15105b620fcfbfc
john Cervantes <johncamilocervantes@gmail.com>
2022-01-13 15:02:35 -0600

    fix styling issues on cart, working on making navigation mobile friendly

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,57 +1,68 @@
 import React from "react";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Link } from "react-router-dom";
 import "./cart.css";
-const Cart = ({
-  cart ,
-  handleUpdateCartQty,
-  handleRemoveFromCart
-        }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart }) => {
   const Emptycart = () => (
     <div className="empty_cart">
       <h1 className="heading">you have no items in your cart</h1>
-      <h1 className="heading"><Link to="/products">start adding some!</Link>  </h1>
-
+      <h1 className="heading">
+        <Link to="/products">start adding some!</Link>{" "}
+      </h1>
     </div>
   );
 
   const Filledcart = () => (
     <div className="filled_cart">
       <div className="filled_cart_container">
-      <table>
-      <tr>
-                <th>Product</th>
-                <th className="qty">Quantity</th>
-                <th>Subtotal</th>
-              </tr>
-      </table>
-        {cart.line_items.map((item) => (
-          <div className="cart_item" key={item.id}>
-            <table>
-              <tr>
-                <td>
-                  <div className="cart_info">
-                    <img src={item.image.url} alt="" />
-                    <div>
-                      <p>{item.name}</p>
-                      <small>price: {item.price.formatted_with_symbol}</small>
-                      <br />
-                      <button onClick={()=> handleRemoveFromCart(item.id)}>remove</button>
-                    </div>
+        <table>
+          <tr>
+            <th>Product</th>
+            <th className="qty">Quantity</th>
+            <th>Subtotal</th>
+          </tr>
+
+          {cart.line_items.map((item) => (
+            <tr className="cart_item" key={item.id}>
+              <td >
+                <div className="cart_info">
+                  <img src={item.image.url} alt="" />
+                  <div>
+                    <p>{item.name}</p>
+                    <small>price: {item.price.formatted_with_symbol}</small>
+                    <br />
+                    <button onClick={() => handleRemoveFromCart(item.id)}>
+                      remove
+                    </button>
                   </div>
-                </td>
-                <td className="cart_item_quantity">
-                  <button className="btn" onClick={() => handleUpdateCartQty( item.id, item.quantity +1)}>+</button>
-                  <p>{item.quantity}</p>
-                  <button className="btn" onClick={() => handleUpdateCartQty(item.id, item.quantity -1)}>-</button>
-                </td>
-                <td>${item.quantity*item.price.formatted}.00</td>
-              </tr>
-            </table>
-          </div>
-        ))}
-        
+                </div>
+              </td>
+              <td className="cart_item_quantity">
+                <button
+                  className="btn"
+                  onClick={() =>
+                    handleUpdateCartQty(item.id, item.quantity + 1)
+                  }
+                >
+                  +
+                </button>
+                <p>{item.quantity}</p>
+                <button
+                  className="btn"
+                  onClick={() =>
+                    handleUpdateCartQty(item.id, item.quantity - 1)
+                  }
+                >
+                  -
+                </button>
+              </td>
+              <td >
+              ${item.quantity * item.price.formatted}.00</td>
+            </tr>
+          ))}
+        </table>
+
         <div className="total_price">
           <table>
             <tr>
@@ -67,7 +78,9 @@ const Cart = ({
               <td>{cart.subtotal.formatted_with_symbol}</td>
             </tr>
 
-            <Link to="/checkout"><button className="btn">Checkout</button></Link>
+            <Link to="/checkout">
+              <button className="btn">Checkout</button>
+            </Link>
           </table>
         </div>
       </div>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,38 +1,62 @@
-import React from 'react'
-import "./Header.css"
-import { Link } from 'react-router-dom'
-import { HashLink } from 'react-router-hash-link';
+import React, { useState } from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
-const Header = ({totalitems}) => {
-    return (
-        <header className="header">
-        
-        <a href className="logo">
-            <i className="fas fa-ghost"></i>
-            logo
-        </a>
+const Header = ({ totalitems }) => {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
-        <nav className="navbar">
-            <HashLink to="/#home" className="active">Home</HashLink>
-            <HashLink to="/#hot">top picks</HashLink>
-            <HashLink to="/#about">About</HashLink>
-            <HashLink to="/Products">Products</HashLink>
-            <HashLink to="/#arrivals">arrivals</HashLink>
-            <HashLink to="/#contact">contact</HashLink>
-        </nav>
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
 
-        <div id="menu-btn" className="fas fa-bars"></div>
+  return (
+    <header className="header">
+      <a href className="logo">
+        <i className="fas fa-ghost"></i>
+        logo
+      </a>
 
-        <div className="icons">
-            <div id="menu-btn" className="fas fa-bars"></div>
-            <div id="search-btn" className="fas fa-search"></div>
-            <Link to="/Cart" className="fas fa-shopping-cart"><span className="cart_quantity">{totalitems}</span></Link>
-            <a href className="fas fa-user"></a>
-        </div>
+      <nav className="navbar">
+        <HashLink to="/#home" className="active">
+          Home
+        </HashLink>
+        <HashLink to="/#hot">top picks</HashLink>
+        <HashLink to="/#about">About</HashLink>
+        <HashLink to="/Products">Products</HashLink>
+        <HashLink to="/#arrivals">arrivals</HashLink>
+        <HashLink to="/#contact">contact</HashLink>
+      </nav>
 
-    </header> 
- 
-    )
-}
+      <div className="icons">
+        <div
+          id="menu-btn"
+          className="fas fa-bars"
+          onClick={toggleHamburger}
+        ></div>
+        <div id="search-btn" className="fas fa-search"></div>
+        <Link to="/Cart" className="fas fa-shopping-cart">
+          <span className="cart_quantity">{totalitems}</span>
+        </Link>
+        <a href className="fas fa-user"></a>
+      </div>
 
-export default Header
+      <div
+        className="navigation"
+        style={{ display: hamburgerOpen ? "inline" : "none" }}
+      >
+        <HashLink to="/#home" className="active" style={{ fontSize: "17px",marginRight: "5px" }}>
+          Home
+        </HashLink>
+
+        <HashLink to="/#hot" style={{ fontSize: "17px",marginRight: "5px" }}>top picks</HashLink>
+        <HashLink to="/#about" style={{ fontSize: "17px",marginRight: "5px" }}>About</HashLink>
+        <HashLink to="/Products" style={{ fontSize: "17px",marginRight: "5px" }}>Products</HashLink>
+        <HashLink to="/#arrivals" style={{ fontSize: "17px",marginRight: "5px" }}>arrivals</HashLink>
+        <HashLink to="/#contact" style={{ fontSize: "17px",marginRight: "5px" }}>contact</HashLink>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
